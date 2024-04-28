191ec1f2c13ca23f3cd5ee530b812d9d8d1f7a56
Kocimjan <tbobojon25@gmail.com>
2024-04-28 13:38:04 +0500

    update proj

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
 import sqlite3
+import time
 from telebot import types
 from bot import bot
 import requests
-
+import schedule
+import threading
+import config
 
 class database:
 
@@ -32,31 +35,62 @@ def update_location_next(message):
     db = database()
     user_id = message.from_user.id
     chat_id = message.chat.id
-    lat = message.location.latitude
-    lon = message.location.longitude
-    try:
-        db.execute("UPDATE user SET lat=?, lon=? WHERE user_id=?", lat, lon, user_id)
-        db.commit()
-        bot.send_message(chat_id, "")
-    except Exception as e:
-        bot.send_message(chat_id, f"Ошибка при сохранении ваших координат: {e}")
-    finally:
-        db.close()
+    if message.content_type == 'location':
+        lat = message.location.latitude
+        lon = message.location.longitude
+        try:
+            db.execute("UPDATE user SET lat = ?, lon = ? WHERE user_id = ?", lat, lon, user_id)
+            db.commit()
+            bot.send_message(chat_id, "Ваша локация успешно обновлено.")
+        except Exception as e:
+            bot.send_message(chat_id, f"Ошибка при сохранении ваших координат попробуйте позже или сообщите мне @TB4TE")
+        finally:
+            db.close()
+    elif message.content_type == 'text':
+        city = message.text
+        try:
+            db.execute("UPDATE user SET city = ?", city)
+            db.commit()
+            bot.send_message(chat_id, "Ваша локация успешно обновлено.")
+        except:
+            bot.send_message(chat_id, f"Ошибка при сохранении ваших координат попробуйте позже или сообщите мне @TB4TE")
+        finally:
+            db.close()
 
 
 def set_location_next(message):
     db = database()
     user_id = message.from_user.id
-    lat = message.location.latitude
-    lon = message.location.longitude
-    try:
-        username = message.from_user.username
-        db.execute("INSERT INTO user (username, tg_id, lat, lon) VALUES (?, ?, ?)", username, user_id, lat, lon)
-    except:
-        db.execute("INSERT INTO user (tgid, lat, lon) VALUES (?, ?, ?)", user_id, lat, lon)
-    finally:
-        db.commit()
-        db.close()
+    chat_id = message.chat.id
+    if message.content_type == 'location':
+        lat = message.location.latitude
+        lon = message.location.longitude
+        url = f'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&appid={config.api_key}'
+        convert_response = requests.get(url)
+        data = convert_response.json()[0]
+        city = data['local_names']['ru']
+        try:
+            username = message.from_user.username
+            db.execute("INSERT INTO user (username, tg_id, city, lat, lon) VALUES (?, ?, ?, ?, ?)", username, user_id, city, lat, lon)
+            bot.send_message(chat_id, "Ваша локация успешно установлена.")
+        except:
+            db.execute("INSERT INTO user (tgid, lat, lon) VALUES (?, ?, ?)", user_id, lat, lon)
+            bot.send_message(chat_id, "Ваша локация успешно установлена.")
+        finally:
+            db.commit()
+            db.close()
+    elif message.content_type == 'text':
+        city = message.text
+        try:
+            username = message.from_user.username
+            db.execute("INSERT INTO user (username, tg_id, city) VALUES (?, ?, ?)", username, user_id, city)
+            bot.send_message(chat_id, "Ваша локация успешно установлена.")
+        except Exception as e:
+            db.execute("INSERT INTO user (username, tg_id, city) VALUES (?, ?)",  user_id, city)
+            bot.send_message(chat_id, "Ваша локация успешно установлена.")
+        finally:
+            db.commit()
+            db.close()
 
 
 def get_weather(api_key, city=None, lat=None, lon=None):
@@ -77,3 +111,67 @@ def get_weather(api_key, city=None, lat=None, lon=None):
     else:
         return 'Ошибка при получении данных о погоде', response_weather
 
+
+def user_exist(message):
+    db = database()
+    user_id = message.from_user.id
+    user = db.execute("SELECT * FROM user WHERE tg_id = ?", user_id)
+    db.close()
+    if user:
+        return True
+    else:
+        return False
+
+def set_notification_frequency(message):
+    chat_id = message.chat.id
+    try:
+        frequency = int(message.text)
+        if frequency <= 0:
+            raise ValueError
+        bot.send_message(chat_id, "Теперь укажите время для каждой отправки уведомления (в формате ЧЧ:ММ), через запятую:")
+        bot.register_next_step_handler(message, set_notification_times, frequency)
+    except ValueError:
+        bot.send_message(chat_id, "Пожалуйста, введите целое положительное число.")
+        bot.register_next_step_handler(message, set_notification_frequency)
+
+
+def set_notification_times(message, frequency):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    db = database()
+    times_str = message.text.split(',')
+    city = db.execute("SELECT city FROM user WHERE tg_id = ?", user_id)[0]
+    try:
+        times = []
+        for time_str in times_str:
+            hours, minutes = map(int, time_str.split(':'))
+            if hours < 0 or hours > 23 or minutes < 0 or minutes > 59:
+                raise ValueError
+            times.append(time_str)
+        times_str = ','.join(times)
+        db.execute("INSERT OR REPLACE INTO user_notifications (chat_id, frequency, times, city) VALUES (?, ?, ?, ?)", chat_id, frequency, times_str, city[0])
+        db.commit()
+        db.close()
+        bot.send_message(chat_id, "Уведомления успешно настроены!")
+    except ValueError:
+        bot.send_message(chat_id, "Пожалуйста, введите время в правильном формате (ЧЧ:ММ) и убедитесь, что оно корректно.")
+        bot.register_next_step_handler(message, set_notification_times, frequency)
+
+# Функция для выполнения планирования уведомлений
+def schedule_notifications():
+    db = database()
+    while True:
+        current_time = time.strftime('%H:%M')
+        rows = db.execute("SELECT chat_id, times, city FROM user_notifications")
+        if rows:
+            for row in rows:
+                chat_id = row[0]
+                times = row[1].split(',')
+                city = row[2]
+                if current_time in times:
+                    bot.send_message(chat_id, get_weather(config.api_key, city=city))
+        time.sleep(60)  # Проверка каждую минуту
+
+# Запуск потока для выполнения планирования уведомлений в фоне
+thread = threading.Thread(target=schedule_notifications)
+thread.start()
--- personal_actions.py
+++ personal_actions.py
@@ -9,57 +9,60 @@ def start(message):
     bot.send_message(message.chat.id, """Привет! Я - ваш надежный погодный помощник! Хотите всегда быть в курсе текущей погоды в любом 
 уголке мира? Тогда доверьтесь мне!
 
-🌤️ Просто отправьте мне название вашего города или поделитесь своей геолокацией, и я предоставлю вам актуальную информацию о температуре, облачности, влажности и скорости ветра.
+🌤️ Просто отправьте мне название вашего города или поделитесь своей геолокацией, и я предоставлю вам актуальную информацию о погоде.
 
-📡 Также, чтобы всегда быть в курсе изменений погоды, вы можете подписаться на рассылку актуальной погоды с помощью команды /subscribe.
+📡 Также, чтобы всегда быть в курсе изменений погоды, вы можете подписаться на рассылку актуальной погоды с помощью команды /set_notification.
 
 🔄 Если вы хотите обновить своё местоположение или добавить новое, воспользуйтесь командами /update_location и /set_location соответственно.
 
 Не позволяйте погоде влиять на ваши планы! Доверьтесь мне и будьте в курсе всех изменений!
 
 👨‍💻 Этот бот разработан @TBATE. Если у вас есть какие-либо вопросы или предложения, не стесняйтесь обращаться!""")
+    
+    if not functions.user_exist(message):
+        set_location(message)
 
 
-# Обработчик команды /get_location
 @bot.message_handler(commands=['update_location'])
 def update_location(message):
-    # Создание клавиатуры с кнопкой "Поделиться местоположением"
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button = types.KeyboardButton(text="Отправить местоположение", request_location=True)
     keyboard.add(button)
 
-    # Отправка сообщения с запросом местоположения и клавиатурой
-    bot.send_message(message.chat.id, "Поделись своим местоположением, нажав на кнопку ниже:", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Отправь названия города или, поделись своим местоположением, нажав на кнопку ниже:",
+                     reply_markup=keyboard)
+    bot.register_next_step_handler(message, functions.update_location_next)
 
-
+# Обработчик команды для настройки локации
 @bot.message_handler(commands=['set_location'])
 def set_location(message):
-    # Создание клавиатуры с кнопкой "Поделиться местоположением"
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button = types.KeyboardButton(text="Отправить местоположение", request_location=True)
     keyboard.add(button)
 
-    # Отправка сообщения с запросом местоположения и клавиатурой
-    bot.send_message(message.chat.id, "Поделись своим местоположением, нажав на кнопку ниже:", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Отправь названия города, или поделись своим местоположением, нажав на кнопку ниже:", reply_markup=keyboard)
     bot.register_next_step_handler(message, functions.set_location_next)
 
 
-@bot.message_handler(commands=['subscribe'])
-def subscribe(message):
-    db = functions.database()
+# Обработчик команды для настройки уведомлений
+@bot.message_handler(commands=['set_notification'])
+def set_notification(message):
     chat_id = message.chat.id
-    user_id = message.from_user.id
-    check = db.execute("SELECT user_id FROM subscribe WHERE user_id = ?", user_id)
-    if not check:
-        db.execute("INSERT INTO subscribe (user_id, chat_id) VALUES ((SELECT user_id FROM user WHERE tg_id = ?), ?", chat_id, chat_id)
-        db.commit()
-    db.close()
+    bot.send_message(chat_id, "Сколько раз в день вы хотели бы получать уведомления?")
+    bot.register_next_step_handler(message, functions.set_notification_frequency)
 
 
-# Обработчик для сообщений с типом "location"
-@bot.message_handler(content_types=['location'])
+
+# Обработчик для сообщений с типом "location, text"
+@bot.message_handler(content_types=['location', 'text'])
 def handle_location(message):
     chat_id = message.chat.id
-    lat = message.location.latitude
-    lon = message.location.longitude
-    bot.send_message(chat_id, functions.get_weather(api_key, lat=lat, lon=lon))
+    if message.content_type == 'location':
+        lat = message.location.latitude
+        lon = message.location.longitude
+        bot.send_message(chat_id, functions.get_weather(api_key, lat=lat, lon=lon))
+    elif message.content_type == 'text':
+        bot.send_message(chat_id, functions.get_weather(api_key, city=message.text))
+
+
+
